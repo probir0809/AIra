aca9739d6809c150e1025f4ef2fa9dcd4ffee66d
probir0809 <133147616+probir0809@users.noreply.github.com>
2026-01-22 15:59:22 +0530

    no major change main.py

--- aira/main.py
+++ aira/main.py
@@ -1,18 +1,3 @@
-# from aira.core.llm_loader import AIraModel
-
-# def main():
-#     model = AIraModel()
-#     prompt = """You are a helpful assistant.
-#             User: Explain attention mechanism in simple terms.
-#             Assistant:"""
-#     response = model.generate_text(prompt)
-#     print(f"Prompt: {prompt}")
-#     print(f"Response: {response}")
-
-# if __name__ == "__main__":
-#     main()
-
-
 from aira.core.llm_loader import AIraModel
 from aira.chains.basic_chain import BasicChain
 
